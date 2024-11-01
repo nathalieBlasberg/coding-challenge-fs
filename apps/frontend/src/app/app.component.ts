dcc21d2ea3d8788e2da3dbeff794022af08db40a
Bettina Blasberg <bettina.blasberg@yahoo.de>
2024-11-01 19:41:44 +0100

    add components to app

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { PeopleListComponent } from "./components/people-list/people-list.component";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [PeopleListComponent, RouterModule],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'frontend';
+  title = "Angular Star Wars";
 }
--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Route } from '@angular/router';
+import { Route } from "@angular/router";
+import { PeopleListComponent } from "./components/people-list/people-list.component";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: PeopleListComponent },
+];
