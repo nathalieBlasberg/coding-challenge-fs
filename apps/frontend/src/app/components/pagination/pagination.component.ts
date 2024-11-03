ce54ff70d13fda409f42f669285e6e9fe2986cf2
Bettina Blasberg <bettina.blasberg@yahoo.de>
2024-11-03 17:41:46 +0100

    refactor pagination

---
+++ apps/frontend/src/app/components/pagination/pagination.component.ts
@@ -0,0 +1,53 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-pagination",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./pagination.component.html",
+  styleUrl: "./pagination.component.scss",
+})
+export class PaginationComponent implements OnChanges {
+  @Input() people: number;
+  @Input() search: number;
+  @Input() page: number;
+  @Input() total: number;
+
+  @Output() newPageEvent = new EventEmitter<number>();
+
+  public peopleLength: number;
+  public searchLength: number;
+  public pageCount: number;
+  public totalPages: number;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (changes["people"]) {
+      this.peopleLength = changes["people"].currentValue;
+    }
+
+    if (changes["search"]) {
+      this.searchLength = changes["search"].currentValue;
+    }
+
+    if (changes["page"]) {
+      this.pageCount = changes["page"].currentValue;
+    }
+
+    if (changes["total"]) {
+      this.totalPages = changes["total"].currentValue;
+    }
+  }
+
+  public onPageChange(page: number) {
+    this.newPageEvent.emit(page);
+  }
+}
