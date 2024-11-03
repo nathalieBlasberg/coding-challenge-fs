e95b2214a7b2acd06f5730da5e606d9b548dd5a3
Bettina Blasberg <bettina.blasberg@yahoo.de>
2024-11-03 20:33:24 +0100

    improve validation

--- apps/frontend/src/app/components/header/header.component.ts
+++ apps/frontend/src/app/components/header/header.component.ts
@@ -1,42 +1,53 @@
-import { Component, OnInit } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {
   FormBuilder,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
-  Validators,
-} from "@angular/forms";
-import { Output, EventEmitter } from "@angular/core";
+} from '@angular/forms';
+import { Output, EventEmitter } from '@angular/core';
+import { INVALID, z } from 'zod';
 
 @Component({
-  selector: "app-header",
+  selector: 'app-header',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  templateUrl: "./header.component.html",
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
   @Output() newSearchEvent = new EventEmitter<string>();
 
-  private pattern = "^[a-zA-Z ]*$";
+  private pattern = '^[a-zA-Z ]*$';
+  private schema = z
+    .string()
+    .toLowerCase()
+    .refine((search) => search.match(this.pattern));
 
   public searchForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.setForm();
+  }
+
+  public setForm(): void {
     this.searchForm = this.formBuilder.group({
-      searchName: ["", Validators.pattern(this.pattern)],
+      searchName: [''],
     });
   }
 
   get searchName() {
-    return this.searchForm.get("searchName");
+    return this.searchForm.get('searchName');
   }
 
   public onSearchChange(search: string) {
-    if (search.match(this.pattern)) {
-      this.newSearchEvent.emit(search.toLowerCase());
+    try {
+      this.schema.parse(search);
+      this.newSearchEvent.emit(search);
+    } catch (error) {
+      this.searchName?.setErrors({ INVALID });
     }
   }
 }
